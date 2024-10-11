d1ac6e1938c48a0fc7adeec394c9c98748446c28
cleberson.chagas <cleberson.chagas@syngenta.com>
2024-10-11 16:49:35 -0300

    test hash torrent

--- src/main/java/TorrentInputStream.java
+++ src/main/java/TorrentInputStream.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
 
 public class TorrentInputStream {
@@ -9,11 +11,9 @@ public class TorrentInputStream {
         return Files.readAllBytes(Paths.get(fileName));
     }
 
-    public static String byteArray2Hex(final byte[] hash) {
-        Formatter formatter = new Formatter();
-        for (byte b : hash) {
-            formatter.format("%02x", b);
-        }
-        return formatter.toString();
+    public static byte[] byteArray2Hex(final byte[] hash) throws NoSuchAlgorithmException {
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        return digest.digest(hash);
     }
 }
