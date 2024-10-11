2d04ed41418e5e06b4859db5020d556f8a5ce2a9
cleberson.chagas <cleberson.chagas@syngenta.com>
2024-10-11 16:44:19 -0300

    test hash torrent

--- src/main/java/BencodeEncode.java
+++ src/main/java/BencodeEncode.java
@@ -22,10 +22,10 @@ public class BencodeEncode {
     }
 
     String encodeList(List<Object> toEncode){
-        return "l" +toEncode.stream().map(o -> (encode(o, BencodeType.from(o.getClass().getName())))).collect(Collectors.joining())+"e";
+        return "l" +toEncode.stream().sorted().map(o -> (encode(o, BencodeType.from(o.getClass().getName())))).collect(Collectors.joining())+"e";
     }
     String encodeDic(Map<String, Object> toEncode){
-        return "d" + toEncode.entrySet().stream().map(o -> (encode(o.getKey(), BencodeType.STRING) +encode(o.getValue(), BencodeType.from(o.getValue().getClass().getName())))).collect(Collectors.joining())+"e";
+        return "d" + toEncode.entrySet().stream().sorted().map(o -> (encode(o.getKey(), BencodeType.STRING) +encode(o.getValue(), BencodeType.from(o.getValue().getClass().getName())))).collect(Collectors.joining())+"e";
     }
 
     String encodeString(Object bencodeDecoded){
