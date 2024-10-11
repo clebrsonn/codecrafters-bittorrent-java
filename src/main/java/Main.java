0fe7fcc6e2b9b39cc6816ae444201ff740f00007
cleberson.chagas <cleberson.chagas@syngenta.com>
2024-10-11 17:00:42 -0300

    test hash torrent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
         System.out.println("Tracker URL: " + ((HashMap<String, Object>) decoded).get("announce"));
         System.out.println("Length: " + ((HashMap<String, Object>)((HashMap<String, Object>) decoded).get("info")).get("length"));
-        System.out.println("Info Hash: " + TorrentInputStream.byteArray2Hex(new BencodeEncode().encode(((HashMap<String, Object>) decoded).get("info"), BencodeType.DICTIONARY).getBytes()));
+        System.out.println("Info Hash: " + TorrentInputStream.byteArray2Hex(new BencodeEncode().encode(((HashMap<String, Object>) decoded).get("info"), BencodeType.DICTIONARY)));
 
     } else {
       System.out.println("Unknown command: " + command);
