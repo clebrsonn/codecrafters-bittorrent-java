924fb891b23b74b7c69b437dcbb61ea449e92640
cleberson.chagas <cleberson.chagas@syngenta.com>
2024-10-18 21:30:04 -0300

    test get peers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -67,7 +67,7 @@ public class Main {
 
           System.out.println(new HttpRequests().get((String) ((TreeMap<String, Object>) decoded).get("announce"), Map.ofEntries(
 
-                  Map.entry("info_hash",TorrentInputStream.bytesToHex(outputStream.toByteArray())),
+                  Map.entry("info_hash",TorrentInputStream.hexToSha1(outputStream.toByteArray())),
                   Map.entry("peer_id",  "cbc-1234567890v4f5t6"),
                   Map.entry("port",  "6881"),
                   Map.entry("uploaded",  "0"),
