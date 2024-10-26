27c0ab93aee0eea86eb8c48e3b354ae540c9bfe4
cleberson.chagas <cleberson.chagas@syngenta.com>
2024-10-26 19:01:43 -0300

    test get peers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,14 +30,14 @@ public class Main {
           case "info" -> {
               DigestUtil digestUtil = new DigestUtil();
               var file = digestUtil.readFile(args[1]);
-              BencodeDecode bencodeDecode = new BencodeDecode(file, false);
+              BencodeDecode bencodeDecode = new BencodeDecode(file, true);
               decoded = bencodeDecode.decode();
               final var torrent = Torrent.of((TreeMap<String, Object>) decoded);
 
               System.out.println("Tracker URL: " + torrent.announce());
               System.out.println("Length: " + torrent.info().length());
 
-              System.out.println("Info Hash: " + DigestUtil.bytesToHex(torrent.info().hash()));
+              System.out.println("Info Hash: " + new String(torrent.info().hash(), StandardCharsets.ISO_8859_1));
               System.out.println("Piece Length: " + torrent.info().pieceLength());
 
               System.out.println("Piece Hashes:");
