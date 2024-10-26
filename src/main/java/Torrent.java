8e7168e20ee9e93a81940e92b6934156fa0ac6aa
cleberson.chagas <cleberson.chagas@syngenta.com>
2024-10-26 18:30:28 -0300

    test get peers

--- src/main/java/Torrent.java
+++ src/main/java/Torrent.java
@@ -3,7 +3,7 @@ import java.util.Map;
 public record Torrent(String announce, TorrentInfo info) {
 
     public static Torrent of(Map<String, Object> root) {
-        final var announce = "" +root.get("announce");
+        final var announce = (String) root.get("announce");
         final var info = TorrentInfo.of((Map<String, Object>) root.get("info"));
 
         return new Torrent(announce, info);
