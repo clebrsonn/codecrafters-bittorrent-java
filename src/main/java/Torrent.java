4182a6a9614959a9c98bb4018cd86ea6ef68a7b1
cleberson.chagas <cleberson.chagas@syngenta.com>
2024-10-26 17:51:12 -0300

    test get peers

--- src/main/java/Torrent.java
+++ src/main/java/Torrent.java
@@ -1,9 +1,10 @@
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public record Torrent(String announce, TorrentInfo info) {
 
     public static Torrent of(Map<String, Object> root) {
-        final var announce = (String) root.get("announce");
+        final var announce = new String((byte[]) root.get("announce"), StandardCharsets.ISO_8859_1);
         final var info = TorrentInfo.of((Map<String, Object>) root.get("info"));
 
         return new Torrent(announce, info);
